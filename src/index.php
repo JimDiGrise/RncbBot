d1df262d73d2d43f755739cd5b0044a05f343531
reutov <underwater.aviator@gmail.com>
2017-08-29 19:55:01 +0300

    Fix bug with wrong path to autoloader

--- src/index.php
+++ src/index.php
@@ -5,10 +5,10 @@
     require "api/Commands.php";
     require "api/yandex.php";
 
+    try {
     $tl = new Telegram("https://api.telegram.org/", "bot447665582:AAGEiHixHo5KBV8pp-aaYDhKxisfyu8X6DQ/");
     $location;
         while(1) {
-            try {
                 sleep(3);
                 $command = new Commands($tl);
                 $lastMessage = $tl->getLastMessage();
@@ -16,11 +16,11 @@
                     $tl->confirmMessage();
                     $command->handleCommand($lastMessage);
                 }
-            } catch(Exception $e) {
-                echo "Exception: " . $e;
+                
             }
-
+            
+        } catch(Exception $e) {
+            echo "Exception: " . $e;
         }
-
    
 ?>
